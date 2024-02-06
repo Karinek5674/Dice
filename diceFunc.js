7c3163d911462fa26d40da609e44eac822b37317
Karinek <kohoutek5674@gmail.com>
2024-02-06 07:49:14 +0100

    BasicStart

---
+++ diceFunc.js
@@ -0,0 +1,13 @@
+let kostka1 = 0;
+let kostka2 = 0;
+let kostka3 = 0;
+let kostka4 = 0;
+let kostka5 = 0;
+let kostka6 = 0;
+
+
+
+function randomDice() { //funkce generující číslo od 1 do 6 na random
+	random = Math.floor(Math.random() * 6) + 1
+}
+
