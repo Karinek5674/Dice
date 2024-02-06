0f1141b15d9007027349f9d7ccb29a68ab55c171
Karinek <kohoutek5674@gmail.com>
2024-02-06 08:03:46 +0100

    Funkcni prirazovani hodnot kostkam

--- diceFunc.js
+++ diceFunc.js
@@ -1,3 +1,4 @@
+//jednotlive kostky
 let kostka1 = 0;
 let kostka2 = 0;
 let kostka3 = 0;
@@ -5,9 +6,18 @@ let kostka4 = 0;
 let kostka5 = 0;
 let kostka6 = 0;
 
+//array obsahujici kostky, do ktere se pak budou hodnoty prirazovat a z ni brat 
+const kostky = [kostka1, kostka2, kostka3, kostka4, kostka5, kostka6]
 
+//funkce generující číslo od 1 do 6 na random
+function randomDice() { 
+	return Math.floor(Math.random() * 6) + 1
+}
 
-function randomDice() { //funkce generující číslo od 1 do 6 na random
-	random = Math.floor(Math.random() * 6) + 1
+//funkce, ktera kazde kostce v arrayi priradi hodnotu 1 - 6 
+function priradHodnotuKostkam() { 
+	for (let i = 0; i < kostky.length; i++) {
+		kostky[i] = randomDice()
+	}
 }
 
